4bdb9663dedd8a95d53bb199cdfc1690f2123255
farukhGit <furukh4564@gmail.com>
2020-09-13 14:35:57 +0530

    add javascript function to close button

---
+++ script.js
@@ -0,0 +1,9 @@
+var closeBtn = document.getElementById('cross');
+var container = document.getElementsByClassName('container');
+
+closeBtn.onclick = function(){
+
+    container[0].className = "hide-container";
+}
+
+
